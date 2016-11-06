b95e72d1d897f00c75001144bbc5c0aacdc4fd2b
Mary <oconnor@zoology.ubc.ca>
2016-11-05 18:09:35 -0700

    added crescent beach trouble with cowplot, plot_grid and fonts, can't figure that out. yet.

--- seasonality figures.R
+++ seasonality figures.R
@@ -93,10 +93,23 @@ PB.fig <-
 
 PB.fig
 
+CB <- data %>%
+  filter(., site == "Crescent Beach BC") 
+
+CB.fig <-
+  ggplot(CB, aes(x = month, y = relative.value), group_by(variable)) +
+  theme_minimal() +
+  scale_x_continuous(breaks = CB$month) +
+  geom_jitter(aes(x = month, y = relative.value, color = variable), na.rm = TRUE) +
+  geom_smooth(aes(x = month, y = relative.value, color = variable), se = FALSE) +
+  ggtitle("Crescent Beach")
+
+CB.fig
+
 
 ## all figs at once
-all <- plot_grid(GWI.fig,TW.fig, SF.fig, PB.fig, SJI.fig, nrow = 3, ncol = 2)
+all <- plot_grid(GWI.fig,TW.fig, SF.fig, PB.fig, SJI.fig, CB.fig, nrow = 3, ncol = 2, scale = 0.9, label_size = 9, align = "v")
 
-cowplot::ggsave("allsites.jpg", plot = all)
+cowplot::ggsave("allsites.jpg", plot = all, width = 8, height = 10)
 
 ### ok, this will work to visualize the patterns. next step, try to get our (and others?) seasonal data into a figure...
